bda559a62fcd24c6684e2a92af1b0ef710bc9ee2
Rohith Reddy GK <gkrohithreddy@gmail.com>
2025-10-28 22:13:48 +0530

    Updated app.py accordingly

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from transliterate import load_model, transliterate_word
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -35,4 +36,5 @@ def model_metadata():
     return jsonify(model_info)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
